de11ac7b8d1adfea8c6c4e827a984224cfce4d57
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-01-08 14:00:41 +0000

    fix

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 // db_config.js
 const dbConfig = {
-  connectionString: 'postgres://pernbackenddatabase_user:d3KkNi9Ii8pAYBnnUb40epV0WHWDCSNf@dpg-cmduvh021fec73d6muqg-a/pernbackenddatabase'
+  connectionString: 'postgres://pernbackenddatabase_user:d3KkNi9Ii8pAYBnnUb40epV0WHWDCSNf@dpg-cmduvh021fec73d6muqg-a.frankfurt-postgres.render.com/pernbackenddatabase'
 };
 
 module.exports = dbConfig;
